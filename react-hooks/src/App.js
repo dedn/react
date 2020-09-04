d2b74826790a39204b684f60525778b9a4344672
Yurii Kyrylov <yuriikyrylov@gmail.com>
2020-09-04 09:40:06 +0300

    some ubdates

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {Navbar} from './components/Navbar'
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Profile} from './pages/Profile'
+import {Alert} from './components/Alert'
+import {AlertState} from './context/alert/AlertState'
 
 function App() {
   return (
-    <div className="container pt-4">
-      <h1>Hello World!</h1>
-    </div>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar/>
+        <div className="container pt-4">
+          <Alert alert={{text: 'Test alert'}}/>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/profile/:name" component={Profile}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
